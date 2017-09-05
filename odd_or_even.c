3e8c6ee8fcec033c7b78138ff7aaab413477dc7d
MADHAN KUMAR <30663342+Madhansmart@users.noreply.github.com>
2017-09-05 18:17:46 +0530

    Add files via upload

---
+++ odd_or_even.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+void main()
+{
+    int num;
+    printf("Enter any number");
+    scanf("%d",&num);
+    if(num>=0)
+        printf("Number is positive");
+    else
+        printf("Number is negative");
+}

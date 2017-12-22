1b5170a132e22ff621ee24c3b5993e505708eee3
MADHAN KUMAR <30663342+Madhansmart@users.noreply.github.com>
2017-12-22 13:57:36 +0530

    Add files via upload

---
+++ Power.java
@@ -0,0 +1,11 @@
+import java.util.*;
+public class Power {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner n = new Scanner(System.in);
+		int x = n.nextInt();
+		int y = n.nextInt();
+		System.out.println(Math.pow(x,y));
+	}
+}

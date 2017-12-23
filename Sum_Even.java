9f42356f1426fb144558c1868cfe02da13cb8b8d
MADHAN KUMAR <30663342+Madhansmart@users.noreply.github.com>
2017-12-23 18:01:38 +0530

    Add files via upload

---
+++ Sum_Even.java
@@ -0,0 +1,15 @@
+import java.util.*;
+public class Sum_Even {
+	public static void main(String[] args) {
+	  	 Scanner x = new Scanner(System.in);
+	  	 int a = x.nextInt();
+	  	 int b = x.nextInt();
+	  	 for(int  i = ++a; i<b ; i++)
+	  	 {
+	  		if(i%2 == 0)
+	  		{
+	  			System.out.println(i);
+	  		}
+	  	 }
+	 }
+	}

4b15d0e8e00d1c0d23d1a95d996990539434e83f
MADHAN KUMAR <30663342+Madhansmart@users.noreply.github.com>
2018-01-19 21:58:23 +0530

    Add files via upload

---
+++ Num_reverse.java
@@ -0,0 +1,16 @@
+import java.util.*;
+public class Num_reverse {
+
+	public static void main(String[] args) {
+		Scanner x = new Scanner(System.in);
+		String s1 = "";
+		int N = x.nextInt();
+		String s = ""+N;
+		for(int i=s.length()-1;i>=0;i--)
+		{
+			s1 = s1 + s.charAt(i);
+		}
+		System.out.println(s1);
+	}
+
+}

9e41658981517644b5d5dbaf5b504c915e833608
MADHAN KUMAR <30663342+Madhansmart@users.noreply.github.com>
2018-02-01 18:19:30 +0530

    Add files via upload

---
+++ Largest_array_element.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+public class Largest_array_element {
+
+	public static void main(String[] args) {
+		Scanner x = new Scanner(System.in);
+		int N = x.nextInt();
+		int a[] = new int[N];
+		for(int i=0;i<N;i++)
+		{
+			a[i] = x.nextInt();
+		}
+		int small = 0;
+		for(int i=1;i<N;i++)
+		{
+			if(a[small] <=a[i])
+			{
+				small = i;
+			}
+		}
+		System.out.println(a[small]);
+
+	}
+
+}
